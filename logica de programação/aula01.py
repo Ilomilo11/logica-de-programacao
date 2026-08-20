6f0d5354df64d5b9fad00856ff76219edd4b30df
Paulo <164180836+Ilomilo11@users.noreply.github.com>
2026-08-20 12:28:35 -0300

    Add files via upload

---
+++ logica de programação/aula01.py
@@ -0,0 +1,17 @@
+#1 programa
+#nome = input("insira seu nome: ")
+#print(f"Olá, {nome}")
+
+#programa 2
+
+#n1 = int(input("insira a nota: "))
+#n2 = int(input("insira a nota: "))
+#media = (n1+n2)/2
+#print(f"sua media é: {media}")
+
+#programa 3
+
+preco = float(input("insira o preco do produto: "))
+
+desconto = preco - (preco * 0.5) 
+print(f"o preco do seu produtop com desconto é: {desconto}")
